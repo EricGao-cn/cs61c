d7eac922a6a6bd6948494f9f8741567fd5afd9bd
Eric Gao <gaoyuxuan.cn@gmail.com>
2025-04-04 19:19:23 +0800

    feat:lab07 finished

--- lab07/ex2.c
+++ lab07/ex2.c
@@ -33,20 +33,54 @@ double dotp_reduction(double* x, double* y, int arr_size) {
     return global_sum;
 }
 
+// double dotp_manual_reduction(double* x, double* y, int arr_size) {
+//     double global_sum = 0.0;
+//     double sums[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+//     #pragma omp parallel
+//     {
+//         int num_threads = omp_get_num_threads();
+//         int thread_id = omp_get_thread_num();
+//         double local_sum = 0.0;  // 每个线程的局部和
+        
+//         for (int i = thread_id; i < arr_size; i += num_threads) {
+//             local_sum += x[i] * y[i];
+//         }
+
+//         #pragma omp critical
+//         {
+//             sums[thread_id % 8] += local_sum;
+//         }
+//     }
+//     global_sum = sums[0] + sums[1] + sums[2] + sums[3] +
+//                  sums[4] + sums[5] + sums[6] + sums[7];
+//     return global_sum;
+// }
+
 // Manual Reduction
 double dotp_manual_reduction(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     // TODO: Implement this function
     // Do NOT use the `reduction` directive here!
-    const int NUM_THREADS = 2;
+    int NUM_THREADS = 0;
+    #pragma omp parallel
+    {
+        #pragma omp single
+        {
+        NUM_THREADS = omp_get_num_threads();
+        }
+    }
     double sum[NUM_THREADS];
-#pragma omp critical
+    for (int i = 0; i < NUM_THREADS; i++) sum[i] = 0;
+#pragma omp parallel
     {
       int id = omp_get_thread_num();
+      double local_sum = 0.0;
       for (int i = id; i < arr_size; i += NUM_THREADS){
-           sum[id] = x[i] * y[i];
+           local_sum += x[i] * y[i];
       }
+      sum[id] = local_sum;
   }
     for (int i = 0; i < NUM_THREADS; i++) global_sum += sum[i];
     return global_sum;
 }
+
